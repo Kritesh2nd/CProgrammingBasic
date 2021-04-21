c5148a91df8c63672c0ccbc25f33420b4275ae78
Kritesh2nd <kriteshthapa2nd@gmail.com>
2021-04-21 18:30:03 +0545

    Print table from 1~10 using nested loop

---
+++ table1to10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    system("cls");
+    int a,b,c,d,e;
+    for(a=1;a<=10;a++){
+        for(b=1;b<=10;b++){
+            printf("\n%d * %d = %d",a,b,a*b);
+        }
+        printf("\n");
+    }
+} 
