2a263326d7585fdc7129431b9e867107e0860a4f
Kritesh2nd <kriteshthapa2nd@gmail.com>
2021-04-22 19:21:44 +0545

    Printing sum of 100 natural numbers

---
+++ sumof100.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+    //WAP to find the sum of first 100 natural number
+    system("cls");
+    int a,b=0;
+    for(a=1;a<=100;a++){
+        b=b+a; 
+    }
+    printf("The sum of first 100 natural number is %d ",b);
+}
